ef647e12c157ddcaaf660afacb48b1706daad92b
Jeosafá Ferreira <a.a.j.d@hotmail.com>
2023-11-10 19:40:14 -0300

    Atualizando

--- src/App.tsx
+++ src/App.tsx
@@ -237,32 +237,6 @@ export default function App() {
                     {/* .sidebar */}
                 </aside>
 
-<<<<<<< HEAD
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="teste" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-=======
                 <Routes>
                     <Route path="/" element={<Dashboard />} />
                     <Route path="/carteira" element={<Carteira />} />
@@ -283,5 +257,4 @@ export default function App() {
             </div>
         </BrowserRouter>
     );
->>>>>>> main
 }
