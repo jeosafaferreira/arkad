cd5b6ebf50d52fd937556ddb5b88a06e48172162
Avv <matheusavelino10@edu.unifor.br>
2023-11-24 17:51:13 -0300

    Template ok

--- src/views/Carteira/Carteira.tsx
+++ src/views/Carteira/Carteira.tsx
@@ -1,7 +1,10 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChartSimple } from "@fortawesome/free-solid-svg-icons";
+
 export default function Carteira() {
     return (
         // Content Wrapper. Contains page content
-        <div className="content-wrapper">
+        <nav className="content-wrapper">
             {/* Content Header (Page header) */}
             <section className="content-header">
                 <div className="container-fluid">
@@ -23,30 +26,59 @@ export default function Carteira() {
             </section>
 
             {/* Main content */}
-            <section className="content">
-                {/* Default box */}
+            <div className="main-content">
+                <div className="salaryInfo">
+                    <div className="ml-4 mt-2">
+                        <h6 className="salaryInfo-text mt-4">Mês Atual</h6>
+                        <h2 className="mt-4">R$ 2500,00</h2>
+                    </div>
+                    <FontAwesomeIcon className="mt-auto mb-auto" icon={faChartSimple} size="2xl" style={{ color: "#005eff" }} />
+                    <hr className="hrVertical"></hr>
+                </div>
                 <div className="card">
-                    <div className="card-header">
-                        <h3 className="card-title">Title</h3>
-
-                        <div className="card-tools">
-                            <button type="button" className="btn btn-tool" data-card-widget="collapse" title="Collapse">
-                                <i className="fas fa-minus"></i>
-                            </button>
-                            <button type="button" className="btn btn-tool" data-card-widget="remove" title="Remove">
-                                <i className="fas fa-times"></i>
-                            </button>
+                    <div className="card-inner">
+                        <div className="front">
+                            <img src="https://i.ibb.co/PYss3yv/map.png" className="map-img"></img>
+                            <div className="row">
+                                <img src="https://i.ibb.co/G9pDnYJ/chip.png" width="60px"></img>
+                                <img src="https://i.ibb.co/WHZ3nRJ/visa.png" width="60px"></img>
+                            </div>
+                            <div className="row card-no">
+                                <p>5244</p>
+                                <p>2150</p>
+                                <p>8252</p>
+                                <p>6420</p>
+                            </div>
+                            <div className="row card-holder">
+                                <p>CARD HPLDER</p>
+                                <p>VALID TILL</p>
+                            </div>
+                            <div className="row name">
+                                <p>JOE ALISON</p>
+                                <p>10 / 25</p>
+                            </div>
+                        </div>
+                        <div className="back">
+                            <img src="https://i.ibb.co/PYss3yv/map.png" className="map-img"></img>
+                            <div className="bar"></div>
+                            <div className="row card-cvv">
+                                <div>
+                                    <img src="https://i.ibb.co/S6JG8px/pattern.png"></img>
+                                </div>
+                                <p>824</p>
+                            </div>
+                            <div className="row card-text">
+                                <p>this is a virtual card design using HTML and CSS. You can aslo design something like this.</p>
+                            </div>
+                            <div className="row signature">
+                                <p>CUSTOMER SIGNATURE</p>
+                                <img src="https://i.ibb.co/WHZ3nRJ/visa.png" width="80px"></img>
+                            </div>
                         </div>
                     </div>
-                    <div className="card-body">Start creating your amazing application!</div>
-                    {/* /.card-body */}
-                    <div className="card-footer">Footer</div>
-                    {/* /.card-footer */}
                 </div>
-                {/* /.card */}
-            </section>
-            {/* /.content */}
-        </div>
+            </div>
+        </nav>
         //   /.content-wrapper
     );
 }
