c79260fe506112ca939aab230b12ef1ae0e19300
Jeosafá Ferreira <a.a.j.d@hotmail.com>
2023-12-01 17:14:12 -0300

    Páginas atualizadas

--- src/views/Carteira/Carteira.tsx
+++ src/views/Carteira/Carteira.tsx
@@ -1,52 +1,107 @@
+import Footer from "../Components/Footer";
+import SideBar from "../Components/SideBar";
+import TopBar from "../Components/TopBar";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+import "./Carteira.css";
+import { Link } from "react-router-dom";
 export default function Carteira() {
+    const [cartoes, setCartoes] = useState([]);
+    const getCartoes = async () => {
+        const res = await axios.get(`http://localhost:8081/cartoes/${localStorage.getItem("usuario_id")}`);
+        console.log(res.data);
+        setCartoes(res.data);
+    };
+
+    useEffect(() => {
+        getCartoes();
+    }, []);
+
     return (
-        // Content Wrapper. Contains page content
-        <div className="content-wrapper">
-            {/* Content Header (Page header) */}
-            <section className="content-header">
-                <div className="container-fluid">
-                    <div className="row mb-2">
-                        <div className="col-sm-6">
-                            <h1>Carteira</h1>
-                        </div>
-                        <div className="col-sm-6">
-                            <ol className="breadcrumb float-sm-right">
-                                <li className="breadcrumb-item">
-                                    <a href="#">Home</a>
-                                </li>
-                                <li className="breadcrumb-item active">Carteira</li>
-                            </ol>
-                        </div>
-                    </div>
-                </div>
-                {/* /.container-fluid */}
-            </section>
-
-            {/* Main content */}
-            <section className="content">
-                {/* Default box */}
-                <div className="card">
-                    <div className="card-header">
-                        <h3 className="card-title">Title</h3>
-
-                        <div className="card-tools">
-                            <button type="button" className="btn btn-tool" data-card-widget="collapse" title="Collapse">
-                                <i className="fas fa-minus"></i>
-                            </button>
-                            <button type="button" className="btn btn-tool" data-card-widget="remove" title="Remove">
-                                <i className="fas fa-times"></i>
-                            </button>
+        <>
+            <TopBar />
+            <SideBar />
+            {/* Content Wrapper. Contains page content*/}
+            <div className="content-wrapper">
+                {/* Content Header (Page header) */}
+                <section className="content-header">
+                    <div className="container-fluid">
+                        <div className="row mb-2">
+                            <div className="col-sm-6" style={{ display: "flex" }}>
+                                <div>
+                                    <h1>Carteira</h1>
+                                </div>
+                                <div style={{ marginLeft: 20 }}>
+                                    <Link to="/novoCartao">
+                                        <button type="button" className="btn btn-success">
+                                            Cadastrar Cartão
+                                        </button>
+                                    </Link>
+                                </div>
+                            </div>
+                            <div className="col-sm-6">
+                                <ol className="breadcrumb float-sm-right">
+                                    <li className="breadcrumb-item">
+                                        <a href="#">Home</a>
+                                    </li>
+                                    <li className="breadcrumb-item active">Carteira</li>
+                                </ol>
+                            </div>
                         </div>
                     </div>
-                    <div className="card-body">Start creating your amazing application!</div>
-                    {/* /.card-body */}
-                    <div className="card-footer">Footer</div>
-                    {/* /.card-footer */}
-                </div>
-                {/* /.card */}
-            </section>
-            {/* /.content */}
-        </div>
+                    {/* /.container-fluid */}
+                </section>
+
+                {/* Main content */}
+                <section className="content">
+                    {/* /.card */}
+                    <div style={{ display: "flex" }}>
+                        {cartoes.map((cartao) => (
+                            <div className="card" style={{ marginRight: 10 }}>
+                                <div className="card-inner">
+                                    <div className="front">
+                                        <img src="https://i.ibb.co/PYss3yv/map.png" className="map-img"></img>
+                                        <div className="row">
+                                            <img src="https://i.ibb.co/G9pDnYJ/chip.png" width="60px"></img>
+                                            <img src="https://i.ibb.co/WHZ3nRJ/visa.png" width="60px"></img>
+                                        </div>
+                                        <div className="row card-no">
+                                            <p>{cartao.numero}</p>
+                                        </div>
+                                        <div className="row card-holder">
+                                            <p>{cartao.nome_impresso}</p>
+                                            <p>{cartao.vencimento}</p>
+                                        </div>
+                                    </div>
+                                    <div className="back">
+                                        <img src="https://i.ibb.co/PYss3yv/map.png" className="map-img"></img>
+                                        <div className="bar"></div>
+                                        <div className="row card-cvv">
+                                            <div>
+                                                <img src="https://i.ibb.co/S6JG8px/pattern.png"></img>
+                                            </div>
+                                            <p></p>
+                                        </div>
+                                        <div className="row card-text">
+                                            <p></p>
+                                        </div>
+                                        <div className="row signature">
+                                            <p>CUSTOMER SIGNATURE</p>
+                                            <img src="https://i.ibb.co/WHZ3nRJ/visa.png" width="80px"></img>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+
+                    {/* /.card */}
+                </section>
+                {/* /.content */}
+            </div>
+            <Footer />
+        </>
         //   /.content-wrapper
     );
 }
--- src/views/Suporte/Suporte.tsx
+++ src/views/Suporte/Suporte.tsx
@@ -1,113 +1,114 @@
-import './Suporte.css'
-import InputMask from 'react-input-mask';
-import email_icon from './Suporte_Icon/email_icon.png'
-import phone_icon from './Suporte_Icon/phone_icon.png'
-import { Call, Sms } from 'iconsax-react';
+import "./Suporte.css";
+import InputMask from "react-input-mask";
+import email_icon from "./Suporte_Icon/email_icon.png";
+import phone_icon from "./Suporte_Icon/phone_icon.png";
+import { Call, Sms } from "iconsax-react";
+import Footer from "../Components/Footer";
+import TopBar from "../Components/TopBar";
+import SideBar from "../Components/SideBar";
 
-export default function Suporte(){
+export default function Suporte() {
     return (
-        // Content Wrapper. Contains page content
-        <div className="content-wrapper">
-            {/* Content Header (Page header) */}
-            <section className="content-header">
-                <div className="container-fluid">
-                    <div className="row mb-2">
-                        <div className="col-sm-6">
-                            <h1>Suporte</h1>
-                        </div>
-                        <div className="col-sm-6">
-                            <ol className="breadcrumb float-sm-right">
-                                <li className="breadcrumb-item">
-                                    <a href="#">Home</a>
-                                </li>
-                                <li className="breadcrumb-item active">Carteira</li>
-                            </ol>
-                        </div>
-                    </div>
-                </div>
-                {/* /.container-fluid */}
-            </section>
-
-            {/* Main content */}
-            <section className="content">
-                {/* Default box */}
-                <div className="card">
-                    <div className="card-header">
-                        <h3 className="card-title">Title</h3>
-
-                        <div className="card-tools">
-                            <button type="button" className="btn btn-tool" data-card-widget="collapse" title="Collapse">
-                                <i className="fas fa-minus"></i>
-                            </button>
-                            <button type="button" className="btn btn-tool" data-card-widget="remove" title="Remove">
-                                <i className="fas fa-times"></i>
-                            </button>
-                        </div>
-                    </div>
-                    <div className="card-body">
-                        <div className='forms'>
-                            <div className='Main-Card'>
-                                <div className='forms-1'>
-                                    <label htmlFor="nomeCompleto">Nome Completo</label>
-                                    <input type="text" id="nomeCompleto" name="nomeCompleto" placeholder='Nome Completo' />
-                                    <div className='forms-3'>
-                                        <label htmlFor="telefone">Telefone</label>
-                                        <InputMask
-                                            mask="+99 (99) 9 9999 9999"
-                                            maskChar="_"
-                                            id="telefone"
-                                            name="telefone"
-                                            placeholder='+55 (__) 9 ____-____'
-                                        />
-                                    </div>
-                                </div>
-                                <div className='forms-2'>
-                                    <label htmlFor='email'>Email</label>
-                                    <input type='email' id='email' name='email' placeholder='Email'></input>
-                                    <div className='forms-4'>
-                                        <label htmlFor='assunto'>Assunto</label>
-                                        <input type='text' id='assunto' name='assunto'placeholder='Assunto'></input>
-                                    </div>
-                                </div>
+        <>
+            <TopBar />
+            <SideBar />
+            <div className="content-wrapper">
+                {/* Content Header (Page header) */}
+                <section className="content-header">
+                    <div className="container-fluid">
+                        <div className="row mb-2">
+                            <div className="col-sm-6">
+                                <h1>Suporte</h1>
                             </div>
-                                <div className='final-forms'>
+                            <div className="col-sm-6">
+                                <ol className="breadcrumb float-sm-right">
+                                    <li className="breadcrumb-item">
+                                        <a href="#">Home</a>
+                                    </li>
+                                    <li className="breadcrumb-item active">Carteira</li>
+                                </ol>
+                            </div>
+                        </div>
+                    </div>
+                    {/* /.container-fluid */}
+                </section>
+
+                {/* Main content */}
+                <section className="content">
+                    {/* Default box */}
+                    <div className="card">
+                        <div className="card-header">
+                            <h3 className="card-title">Title</h3>
+
+                            <div className="card-tools">
+                                <button type="button" className="btn btn-tool" data-card-widget="collapse" title="Collapse">
+                                    <i className="fas fa-minus"></i>
+                                </button>
+                                <button type="button" className="btn btn-tool" data-card-widget="remove" title="Remove">
+                                    <i className="fas fa-times"></i>
+                                </button>
+                            </div>
+                        </div>
+                        <div className="card-body">
+                            <div className="forms">
+                                <div className="Main-Card">
+                                    <div className="forms-1">
+                                        <label htmlFor="nomeCompleto">Nome Completo</label>
+                                        <input type="text" id="nomeCompleto" name="nomeCompleto" placeholder="Nome Completo" />
+                                        <div className="forms-3">
+                                            <label htmlFor="telefone">Telefone</label>
+                                            <InputMask mask="+99 (99) 9 9999 9999" maskChar="_" id="telefone" name="telefone" placeholder="+55 (__) 9 ____-____" />
+                                        </div>
+                                    </div>
+                                    <div className="forms-2">
+                                        <label htmlFor="email">Email</label>
+                                        <input type="email" id="email" name="email" placeholder="Email"></input>
+                                        <div className="forms-4">
+                                            <label htmlFor="assunto">Assunto</label>
+                                            <input type="text" id="assunto" name="assunto" placeholder="Assunto"></input>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="final-forms">
                                     <label htmlFor="mensagem">Mensagem</label>
-                                    <textarea
-                                        id='mensagem'
-                                        name='mensagem'
-                                        placeholder='Digite sua mensagem'
-                                        rows={10}
-                                    ></textarea>
+                                    <textarea id="mensagem" name="mensagem" placeholder="Digite sua mensagem" rows={10}></textarea>
                                 </div>
-                                <div className='button-enviar'>
-                                    <button id='btn-enviar' name='btn-enviar'>Enviar</button>
-                                </div>
-                        </div>
-                        <div className='contatos'>
-                            <div className='email-contato'>
-                                    <Sms className='email_icon'/>
-                                <div className='text-email'>
-                                <strong><h5>Fale conosco via E-mail</h5></strong>
-                                <h6 className='email_arkad'>suporte@arkad.com.br</h6>
+                                <div className="button-enviar">
+                                    <button id="btn-enviar" name="btn-enviar">
+                                        Enviar
+                                    </button>
                                 </div>
                             </div>
-                            <div className='telefone-contato'>
-                                <Call className='phone_icon'/>
-                                <div className='text-telefone'>
-                                    <strong><h5>SAC</h5></strong>
-                                    <h6 className='phone_arkad'>+55 85 3333-0987</h6>
+                            <div className="contatos">
+                                <div className="email-contato">
+                                    <Sms className="email_icon" />
+                                    <div className="text-email">
+                                        <strong>
+                                            <h5>Fale conosco via E-mail</h5>
+                                        </strong>
+                                        <h6 className="email_arkad">suporte@arkad.com.br</h6>
+                                    </div>
+                                </div>
+                                <div className="telefone-contato">
+                                    <Call className="phone_icon" />
+                                    <div className="text-telefone">
+                                        <strong>
+                                            <h5>SAC</h5>
+                                        </strong>
+                                        <h6 className="phone_arkad">+55 85 3333-0987</h6>
+                                    </div>
                                 </div>
                             </div>
                         </div>
+                        {/* /.card-body */}
+                        <div className="card-footer">Footer</div>
+                        {/* /.card-footer */}
                     </div>
-                    {/* /.card-body */}
-                    <div className="card-footer">Footer</div>
-                    {/* /.card-footer */}
-                </div>
-                {/* /.card */}
-            </section>
-            {/* /.content */}
-        </div>
-        //   /.content-wrapper
-    )
+                    {/* /.card */}
+                </section>
+                {/* /.content */}
+            </div>
+            <Footer />
+        </>
+    );
 }
