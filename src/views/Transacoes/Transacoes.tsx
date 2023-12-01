49a8e121cc92e7dccaea12f32e47e7b31cd72975
Rafael-FHP <rafaelholandafer@edu.unifor.br>
2023-11-30 22:01:11 -0300

    Transações

--- src/views/Transacoes/Transacoes.tsx
+++ src/views/Transacoes/Transacoes.tsx
@@ -1,8 +1,82 @@
+import { useState } from 'react';
+import './Transacoes.css';
+interface Transaction {
+    name: string;
+    type: string;
+    date: string;
+    amount: number;
+    completed: boolean;
+}
+
 export default function Transacoes() {
+    const [transactions, setTransactions] = useState<Transaction[]>([
+        {
+            name: 'Netflix',
+            type: 'Entretenimento',
+            date: '01/03/2023',
+            amount: 45.99,
+            completed: true,
+        },
+        {
+            name: 'Amazon',
+            type: 'Entretenimento',
+            date: '02/02/2023',
+            amount: 120.75,
+            completed: true,
+        },
+        {
+            name: 'Kalzone',
+            type: 'Alimentação',
+            date: '20/04/2023',
+            amount: 120.75,
+            completed: true,
+        },
+        {
+            name: 'Madero',
+            type: 'Alimentação',
+            date: '16/05/2023',
+            amount: 120.75,
+            completed: true,
+        },
+        {
+            name: 'Americanas',
+            type: 'Compras',
+            date: '15/06/2023',
+            amount: 120.75,
+            completed: true,
+        },
+        {
+            name: 'Spotify',
+            type: 'Entrenimento',
+            date: '11/07/2023',
+            amount: 14.00,
+            completed: true,
+        },
+        {
+            name: 'Vivara',
+            type: 'Compras',
+            date: '19/08/2023',
+            amount: 500.75,
+            completed: true,
+        },
+        {
+            name: 'Pao-de-Açucar',
+            type: 'Compras',
+            date: '27/09/2023',
+            amount: 80.75,
+            completed: true,
+        },
+        // Adicione outras transações conforme necessário
+    ]);
+
+    const handleTransactionToggle = (index: number) => {
+        const updatedTransactions = [...transactions];
+        updatedTransactions[index].completed = !updatedTransactions[index].completed;
+        setTransactions(updatedTransactions);
+    };
+
     return (
-        // Content Wrapper. Contains page content
         <div className="content-wrapper">
-            {/* Content Header (Page header) */}
             <section className="content-header">
                 <div className="container-fluid">
                     <div className="row mb-2">
@@ -19,34 +93,40 @@ export default function Transacoes() {
                         </div>
                     </div>
                 </div>
-                {/* /.container-fluid */}
             </section>
 
-            {/* Main content */}
             <section className="content">
-                {/* Default box */}
                 <div className="card">
                     <div className="card-header">
-                        <h3 className="card-title">Title</h3>
-
-                        <div className="card-tools">
-                            <button type="button" className="btn btn-tool" data-card-widget="collapse" title="Collapse">
-                                <i className="fas fa-minus"></i>
-                            </button>
-                            <button type="button" className="btn btn-tool" data-card-widget="remove" title="Remove">
-                                <i className="fas fa-times"></i>
-                            </button>
-                        </div>
+                        <h3 className="card-title">Lista de Transações</h3>
                     </div>
-                    <div className="card-body">Start creating your amazing application!</div>
-                    {/* /.card-body */}
+                    <div className="card-body">
+                        {transactions.map((transaction, index) => (
+                            <div key={index} className="transaction">
+                                <p>
+                                    <span className="transaction-label utilizacao">Utilização:</span>
+                                    <span className="transaction-info utilizacao">{transaction.name}</span>
+                                    <span className="transaction-label tipo">Tipo:</span>
+                                    <span className="transaction-info tipo">{transaction.type}</span>
+                                    <span className="transaction-label data">Data:</span>
+                                    <span className="transaction-info data">{transaction.date}</span>
+                                    <span className="transaction-label pagamento">Pagamento:</span>
+                                    <span className="transaction-info pagamento">R$ {transaction.amount}</span>
+                                </p>
+                                <div>
+                                    <button
+                                        className={`btn btn-${transaction.completed ? 'success' : 'secondary'}`}
+                                        onClick={() => handleTransactionToggle(index)}
+                                    >
+                                        {transaction.completed ? 'Completo' : 'Pendente'}
+                                    </button>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
                     <div className="card-footer">Footer</div>
-                    {/* /.card-footer */}
                 </div>
-                {/* /.card */}
             </section>
-            {/* /.content */}
         </div>
-        //   /.content-wrapper
     );
 }
