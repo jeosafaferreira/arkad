62262ff52ee8dccc014dff53c99fe3cdbebe66a4
Avv <matheusavelino10@edu.unifor.br>
2023-11-30 21:47:19 -0300

    Card Credit

--- src/views/Carteira/Carteira.tsx
+++ src/views/Carteira/Carteira.tsx
@@ -24,24 +24,48 @@ export default function Carteira() {
 
             {/* Main content */}
             <section className="content">
-                {/* Default box */}
+                {/* /.card */}
                 <div className="card">
-                    <div className="card-header">
-                        <h3 className="card-title">Title</h3>
-
-                        <div className="card-tools">
-                            <button type="button" className="btn btn-tool" data-card-widget="collapse" title="Collapse">
-                                <i className="fas fa-minus"></i>
-                            </button>
-                            <button type="button" className="btn btn-tool" data-card-widget="remove" title="Remove">
-                                <i className="fas fa-times"></i>
-                            </button>
+                    <div className="card-inner">
+                        <div className="front">
+                            <img src="https://i.ibb.co/PYss3yv/map.png" className="map-img"></img>
+                            <div className="row">
+                                <img src="https://i.ibb.co/G9pDnYJ/chip.png" width="60px"></img>
+                                <img src="https://i.ibb.co/WHZ3nRJ/visa.png" width="60px"></img>
+                            </div>
+                            <div className="row card-no">
+                                <p>5244</p>
+                                <p>2150</p>
+                                <p>8252</p>
+                                <p>6420</p>
+                            </div>
+                            <div className="row card-holder">
+                                <p>CARD HPLDER</p>
+                                <p>VALID TILL</p>
+                            </div>
+                            <div className="row name">
+                                <p>JOE ALISON</p>
+                                <p>10 / 25</p>
+                            </div>
+                        </div>
+                        <div className="back">
+                            <img src="https://i.ibb.co/PYss3yv/map.png" className="map-img"></img>
+                            <div className="bar"></div>
+                            <div className="row card-cvv">
+                                <div>
+                                    <img src="https://i.ibb.co/S6JG8px/pattern.png"></img>
+                                </div>
+                                <p>824</p>
+                            </div>
+                            <div className="row card-text">
+                                <p>this is a virtual card design using HTML and CSS. You can aslo design something like this.</p>
+                            </div>
+                            <div className="row signature">
+                                <p>CUSTOMER SIGNATURE</p>
+                                <img src="https://i.ibb.co/WHZ3nRJ/visa.png" width="80px"></img>
+                            </div>
                         </div>
                     </div>
-                    <div className="card-body">Start creating your amazing application!</div>
-                    {/* /.card-body */}
-                    <div className="card-footer">Footer</div>
-                    {/* /.card-footer */}
                 </div>
                 {/* /.card */}
             </section>
